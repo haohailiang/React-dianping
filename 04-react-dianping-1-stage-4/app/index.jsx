9090487ac5dfec74d75df2162514fd6c1ac6d176
郝海亮 <2298763729@qq.com>
2018-07-07 16:15:03 +0800

    add:route

---
+++ 04-react-dianping-1-stage-4/app/index.jsx
@@ -0,0 +1,8 @@
+import React      from 'react'
+import { render } from 'react-dom'
+import RouteMap   from './router/RouteMap.jsx'
+
+render(
+    <RouteMap/>,
+    document.getElementById('root')
+)
