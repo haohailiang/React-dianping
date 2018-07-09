60a33458e74df198b89909ab9da07d20cd1cd288
郝海亮 <2298763729@qq.com>
2018-07-09 17:53:38 +0800

    03有bug的todolist

--- 03-react-dianping-getready-perf-optimization/app/index.jsx
+++ 03-react-dianping-getready-perf-optimization/app/index.jsx
@@ -1,18 +1,17 @@
-import React from 'react'
+import React      from 'react'
 import { render } from 'react-dom'
+import Todo       from './containers/Todo';
+// 性能测试
+import Perf       from 'react-addons-perf'
 
 // 通用样式
 import './static/css/common.less'
 
-import Todo from './containers/Todo';
-
-// 性能测试
-import Perf from 'react-addons-perf'
 if (__DEV__) {
-    window.Perf = Perf
+	window.Perf = Perf
 }
 
 render(
-    <Todo/>,
-    document.getElementById('root')
+	<Todo/>,
+	document.getElementById('root')
 )
