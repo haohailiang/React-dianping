ff5ac365e80701f335292e5e169336a3cd545975
郝海亮 <2298763729@qq.com>
2018-07-09 17:21:44 +0800

    02-基本框架的搭建[大组件套小组件]

--- 02-react-dianping-getready-react/app/index.jsx
+++ 02-react-dianping-getready-react/app/index.jsx
@@ -1,12 +1,11 @@
-import React from 'react'
+import React      from 'react'
 import { render } from 'react-dom'
+import Hello      from './containers/Hello/';
 
 // 通用样式
 import './static/css/common.less'
 
-import Hello from './containers/Hello/';
-
 render(
-    <Hello/>,
-    document.getElementById('root')
+	<Hello/>,
+	document.getElementById('root')
 )
