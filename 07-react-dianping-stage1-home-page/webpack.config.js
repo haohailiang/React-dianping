b06c4a265fd457cfcb48ce703b10f3a94e80c3c1
郝海亮 <2298763729@qq.com>
2018-07-10 14:47:36 +0800

    课程第七章

--- 07-react-dianping-stage1-home-page/webpack.config.js
+++ 07-react-dianping-stage1-home-page/webpack.config.js
@@ -64,7 +64,6 @@ module.exports = {
         proxy: {
           // 凡是 `/api` 开头的 http 请求，都会被代理到 localhost:3000 上，由 koa 提供 mock 数据。
           // koa 代码在 ./mock 目录中，启动命令为 npm run mock
-          // 这个地址可以修改为测试机上的地址
           '/api': {
             target: 'http://localhost:3000',
             secure: false
